18b4c48342becfe22d126d797e2057a4ddf6a4ea
TregubovAV <5595@rambler.ru>
2025-03-05 19:08:12 +0700

    Тест успешно проходит

--- src/test/java/SetupTest.java
+++ src/test/java/SetupTest.java
@@ -3,7 +3,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class SetupTest {
     @BeforeAll
     public static void setupAll() {
-        WebDriverManager.chromedriver().clearResolutionCache(); // Очистка кэша драйвера
-        WebDriverManager.chromedriver().setup(); // Принудительная установка последней версии
+        WebDriverManager.chromedriver().clearResolutionCache(); 
+        WebDriverManager.chromedriver().setup(); 
     }
 }
